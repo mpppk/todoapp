8db2e0f3a1692f401666af2d85d7cdb71b9d72c8
mpppk <niboshiporipori@gmail.com>
2019-11-13 00:15:12 +0900

    Convert index page to function component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,44 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { sessionActionCreators } from '../actions/session';
 import { todoActionCreators } from '../actions/todo';
 import MyAppBar from '../components/AppBar';
 import Page from '../components/Page';
 import { ITask } from '../domain/todo';
-import { IUser, State } from '../reducer';
+import { State } from '../reducer';
 
-type IndexProps = {
-  user: IUser | null;
-  tasks: ITask[];
-} & typeof sessionActionCreators &
-  typeof todoActionCreators;
-
-class Index extends React.Component<IndexProps> {
-  constructor(props: IndexProps) {
-    super(props);
-    this.handleClickLogout = this.handleClickLogout.bind(this);
-  }
-
-  public componentDidMount(): void {
-    this.props.requestToInitializeFirebase();
-  }
-
-  public async handleClickLogout(): Promise<void> {
-    this.props.requestToLogout();
-  }
-
-  // tslint:disable-next-line member-access
-  render() {
-    return (
-      <div>
-        <MyAppBar
-          user={this.props.user}
-          onClickLogout={this.handleClickLogout}
-        />
-        <Page
-          user={this.props.user}
-          tasks={this.props.tasks}
-          onClickNewTaskButton={this.props.clickNewTaskButton}
-        />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state: State) => {
+const useHandlers = () => {
+  const dispatch = useDispatch();
   return {
-    tasks: state.tasks,
-    user: state.user
+    clickNewTaskButton: (task: ITask) =>
+      dispatch(todoActionCreators.clickNewTaskButton(task)),
+    requestToInitializeFirebase: () => {
+      dispatch(sessionActionCreators.requestToInitializeFirebase());
+    },
+    requestToLogout: () => dispatch(sessionActionCreators.requestToLogout())
   };
 };
 
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    ...bindActionCreators({ ...sessionActionCreators }, dispatch),
-    ...bindActionCreators({ ...todoActionCreators }, dispatch)
-  };
+const useReduxState = () => {
+  const user = useSelector((state: State) => state.user);
+  const tasks = useSelector((state: State) => state.tasks);
+  return { user, tasks };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Index);
+export default () => {
+  const handlers = useHandlers();
+  const state = useReduxState();
+
+  useEffect(handlers.requestToInitializeFirebase, []);
+
+  return (
+    <div>
+      <MyAppBar user={state.user} onClickLogout={handlers.requestToLogout} />
+      <Page
+        user={state.user}
+        tasks={state.tasks}
+        onClickNewTaskButton={handlers.clickNewTaskButton}
+      />
+    </div>
+  );
+};
