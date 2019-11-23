561df862c3f004b0a35a6dc220d9b8dcb6969445
niboshi <niboshiporipori@gmail.com>
2019-11-23 19:24:27 +0900

    Remove counter (#28)

--- sagas/saga.ts
+++ sagas/saga.ts
@@ -1,12 +1,10 @@
 import { all } from '@redux-saga/core/effects';
-import { watchIncrementAsync } from './counter';
 import { sessionWatchers } from './session';
 import { projectWatchers, taskWatchers } from './task';
 import { todoWatchers } from './todo';
 
 export default function* rootSaga() {
   yield all([
-    watchIncrementAsync(),
     ...sessionWatchers,
     ...todoWatchers,
     ...taskWatchers,
