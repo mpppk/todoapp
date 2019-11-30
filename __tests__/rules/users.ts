96af082c545d78d49f3b8388a571bf8ed0d9a1b0
mpppk <niboshiporipori@gmail.com>
2019-11-30 13:34:16 +0900

    Update /projects security rules

--- __tests__/rules/users.ts
+++ __tests__/rules/users.ts
@@ -1,19 +1,37 @@
 import * as firebase from '@firebase/testing';
-import { initDBWithUID } from './projects';
+import * as fs from 'fs';
 
 describe('/users', () => {
-  const uid = 'alice';
-  const db = initDBWithUID(uid);
+  const opt = {
+    auth: {
+      email: 'alice@example.com',
+      uid: 'alice'
+    },
+    projectId: 'user-test-project',
+    rules: fs.readFileSync('firestore.rules', 'utf8')
+  };
+  const db = firebase.initializeTestApp(opt).firestore();
   const userCollection = db.collection('users');
+
+  beforeAll(async () => {
+    await firebase.loadFirestoreRules(opt);
+  });
+
+  beforeEach(async () => {
+    await firebase.clearFirestoreData(opt);
+  });
 
   it('can list all users', async () => {
     await firebase.assertSucceeds(userCollection.get());
-    await firebase.assertSucceeds(userCollection.doc(uid).get());
+    await firebase.assertSucceeds(userCollection.doc(opt.auth.uid).get());
   });
-  it('can write only own profiler', async () => {
+  it('can update own profile', async () => {
     await firebase.assertSucceeds(
-      userCollection.doc(uid).set({ name: 'alice' })
+      userCollection.doc(opt.auth.uid).set({ name: 'alice' })
     );
+  });
+
+  it('can not write not own profile', async () => {
     await firebase.assertFails(
       userCollection.doc('noexistuser').set({ name: 'xxx' })
     );
