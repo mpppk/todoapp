ca3c1eaa5096c4f50a1e5a89dd708be9c4b0a19f
mpppk <niboshiporipori@gmail.com>
2019-12-15 13:58:57 +0900

    Fix

---
+++ functions/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testMatch: ['**/__tests__/**/*.ts?(x)'],
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  }
+};
